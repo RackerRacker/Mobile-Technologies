2e456c0b2a6d460e0bec1ccfb498f16561416e4b
Dash <u3253065@canberra.edu.au>
2026-03-17 13:03:00 +1100

    Week 5 Tutorial

--- app/src/main/java/com/example/mobiletechapp/MainActivity.java
+++ app/src/main/java/com/example/mobiletechapp/MainActivity.java
@@ -7,10 +7,19 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,16 +37,64 @@ public class MainActivity extends AppCompatActivity {
         String msg = extras.getString("message");
         TextView textView = findViewById(R.id.textViewOutput);
         textView.setText(msg);
+
+        DatabaseReference dbref = FirebaseDatabase.getInstance().getReference();
+        dbref.child("example").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String uploadedText = snapshot.getValue().toString();
+                TextView textView = findViewById(R.id.textViewOutput);
+                textView.setText(uploadedText);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        DatabaseReference dbref2 = FirebaseDatabase.getInstance().getReference("UI and Events");
+        dbref2.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                // The snapshot will go through all children in the current reference (UI and Events)
+                String uploadedText = snapshot.getValue().toString();
+                TextView textView = findViewById(R.id.textViewOutput);
+                textView.append("\n" + uploadedText); // get all values
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     public void displayMessage(View view) {
-        final TextView textView = (TextView) findViewById(R.id.textViewOutput);
+        TextView textView = (TextView) findViewById(R.id.textViewOutput);
         EditText editText = (EditText) findViewById(R.id.editTextInput);
-        if (editText.getText().length() == 0)
-            textView.setText("Please enter a message");
-        else
-            textView.setText(editText.getText());
+        textView.setText(editText.getText());
 
-        Toast.makeText(this, "OK button pressed", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Ok button clicked", Toast.LENGTH_SHORT).show();
+
+        String inputText = editText.getText().toString();
+        DatabaseReference dbref = FirebaseDatabase.getInstance().getReference("UI and Events");
+        String childname = dbref.push().getKey();
+        dbref.child(childname).setValue(inputText);
     }
 }
